f76a70adc4d5800f936e41070914af3a5b4a46fc
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-01 21:54:17 -0400

    scaffolded auth, added route for threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,12 +3,11 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
     /** @test */
     public function a_user_can_browse_threads()
     {
