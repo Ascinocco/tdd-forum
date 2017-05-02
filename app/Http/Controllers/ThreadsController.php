24d5ae383ed308ba9a2418f4b6ebfaf76def5ed1
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-01 21:58:56 -0400

    threads test passing! :D

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -15,7 +15,7 @@ class ThreadsController extends Controller
     public function index()
     {
         $threads = Thread::latest()->get();
-        return view('threads.index', compact('thread'));
+        return view('threads.index', compact('threads'));
     }
 
     /**
