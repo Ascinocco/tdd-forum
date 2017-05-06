20d9ab3f709db80e7f0159ce1600c22454bc54c1
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-06 11:17:59 -0400

    finished episode 3 of laracasts series

--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    /**
+     * Defines eloquent relation ship between replies and users
+     * A reply belongs to a user
+     * 
+     * Should note that because we are not calling the function user,
+     * we must specify the foreign key we want for the owner lookup,
+     * which in this case is user_id
+     * 
+     * @return void
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
