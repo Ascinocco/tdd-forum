ee62f69fc72b35d266006e051dfacc0032fbba99
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-06 13:18:54 -0400

    added form to submit reply

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,20 @@
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+               <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                  {{ csrf_field() }}
+                  <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="Add a reply..." rows="5"></textarea>
+                  </div>
+
+                <button type="submit" class="btn btn-default">Post</button>
+               </form>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
