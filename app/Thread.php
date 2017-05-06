b6aef09b8b42648eaeabcbdf95842281b29d0079
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-06 11:07:13 -0400

    added view threads functionality and appropriate tests. Also setup eloquent relationship

--- app/Thread.php
+++ app/Thread.php
@@ -6,8 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    /**
+     * Returns string representation of thread
+     *
+     * @return void
+     */
     public function path()
     {
         return '/threads/' . $this->id;
+    }
+
+    /**
+     * Creates relationship between thread and its replies
+     * This uses eloquent
+     * 
+     * @return void
+     */
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -12,8 +12,7 @@ class ReadThreadsTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-
-        $thread = factory('App\Thread')->create();
+        $this->thread = factory('App\Thread')->create();
     }
 
     public function a_user_can_view_all_threads()
@@ -24,28 +23,27 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_browse_threads()
-    {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-        $response->assertSee($thread->thread->title);
-        $response->assertStatus(200);
-    }
-        
-    /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads/' . $this->thread->id);
-        $response->assertSee($thread->thread->title);
+        $response->assertSee($this->thread->title);
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+        $response->assertSee($this->thread->title);
         $response->assertStatus(200);
     }
 
     /** @test */
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
-
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($reply->body);
+        $response->assertStatus(200);
     }
 }
